ca0023c1690c756ad74f5a0692ed714ee1676d02
Hurkan Dogan <60603139+hurkandogan@users.noreply.github.com>
2021-03-06 17:50:57 +0100

    initial commit

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ const app = express();
 app.use(cors());
 
 // Static Files
-const devEnv = process.env.NODE_ENV;
-if (devEnv == 'prod') {
+const environment = process.env.NODE_ENV;
+if (environment === 'prod') {
+    console.log("Project is in production environment.")
     const path = require('path');
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));   
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+} else {
+    console.log("Project is in development environment.")
 }
 
 app.use(body.urlencoded({extended: true}));
@@ -30,7 +36,6 @@ require('./api/routes/objects')(app);
 require("./api/routes/invoices")(app);
 require("./api/routes/costTypes")(app);
 
-
 app.listen(PORT, function() {
-    console.log("Works on " + PORT);
+    console.log("Server is alive on port: " + PORT);
 });
